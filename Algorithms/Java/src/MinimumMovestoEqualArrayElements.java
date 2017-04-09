1b2a1701da422aa6bd3f8d37fd19ea3476765476
Suleyman Durgut <suleyman-durgut@uiowa.edu>
2017-04-09 01:59:29 -0500

    update

--- Algorithms/Java/src/MinimumMovestoEqualArrayElements.java
+++ Algorithms/Java/src/MinimumMovestoEqualArrayElements.java
@@ -1,5 +1,12 @@
 /**
  * Created by sdurgut on 4/9/17.
+ * <p>
+ * Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal,
+ * where a move is incrementing n - 1 elements by 1.
+ * Input:[1,2,3]
+ * Output:3
+ * Explanation: Only three moves are needed (remember each move increments two elements):
+ * [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
  */
 
 /**
@@ -12,6 +19,7 @@
  */
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * 1,2,3,4
@@ -40,13 +48,44 @@ public class MinimumMovestoEqualArrayElements
 {
 	public static int minMoves(int[] nums)
 	{
-		// brute force
-		Arrays.sort(nums);
-		
+		/*
+		// not that fast solution
+		// sum = array sum, m = # of moves, n = array size, x = final array element value
+		// sum + m*(n-1) = x*n
+		// x = minNum + m
+		// sum - minNum*n = m
+		int sum = IntStream.of(nums).sum(); // new way of sum with Java8, it looks like this step is damn slow!
+		int min = Integer.MAX_VALUE;
+		for (int x : nums) {
+			if (x < min) {
+				min = x;
+			}
+		}
+		return sum - min*nums.length;
+		*/
+
+		// better solution, adding n-1 array elements is same as decreasing elements one by one to smallest element
+		int min = Integer.MAX_VALUE;
+		for (int x : nums)
+		{
+			if (x < min)
+			{
+				min = x;
+			}
+		}
+		int count = 0;
+		for (int x : nums)
+		{
+			count += x - min;
+		}
+		return count;
+
+
 	}
 
 	public static void main(String[] args)
 	{
-
+		int[] test = {1, 2, 3, 4, 5};
+		System.out.println(minMoves(test));
 	}
 }
